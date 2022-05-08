b15545569ec84a614adddf188981e467b4f257fb
Daniel <danielcoeficiente2021@gmail.com>
2022-05-08 11:52:21 -0500

    commit previo evidencia

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function focusProductos() {
+   var x = document.getElementById('productos');
+   x.setAttribute("tabindex", 1);
+   x.focus()
+}
+
+function test() {
+    console.log('test');
+}
+
+function focusServicios() {
+    var x = document.getElementById('servicios');
+    x.setAttribute("tabindex", 1);
+    x.focus()
+ }
+
